4287a4d24d29110cf3bfff925571b827ad07ea82
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-02-17 16:39:37 -0300

    view individual chat

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,38 +1,32 @@
-// import { getApiKey } from "./apiKey.js";
-// import axios from "axios";
-// const axios = require("axios");
-// import axios from "https://cdn.skypack.dev/axios";
-const OPENAI_API_KEY = "1234";
+import { getApiKey } from "../lib/apiKey.js";
 
-// const OPENAI_API_KEY = getApiKey();
+const OPENAI_API_KEY = getApiKey();
 
-// const url = "https://api.openai.com/v1/models";
-// const headers = { Authorization: `Bearer ${OPENAI_API_KEY}` };
-
-// axios
-//   .get(url, { headers })
-//   .then((response) => {
-//     console.log(response.data);
-//   })
-//   .catch((error) => {
-//     console.error("Erro ao fazer a solicitação: ", error);
-//   });
-
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-  apiKey: OPENAI_API_KEY,
-});
-
-openai.models
-  .list()
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.error("Erro ao fazer a solicitação: ", error);
+export const communicateWithOpenAI = async (messages) => {
+  const url = "https://api.openai.com/v1/chat/completions";
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${OPENAI_API_KEY}`,
+  };
+  const body = JSON.stringify({
+    messages: [{ role: "system", content: messages }],
+    model: "gpt-3.5-turbo",
   });
 
-export const communicateWithOpenAI = (messages) => {
-  //Aquí es donde debes implementar la petición con fetch o axios
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: headers,
+      body: body,
+    });
+
+    if (!response.ok) {
+      throw new Error("Erro ao fazer a solicitação.");
+    }
+    const data = await response.json();
+    return data.choices[0].message.content;
+  } catch (error) {
+    console.error("Erro ao fazer a solicitação: ", error);
+    return null;
+  }
 };
