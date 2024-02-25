d16217a2f8405f3a2e7103c2173768d9c80a306f
Lourdilene Souza <lourdilene.souza@gmail.com>
2024-02-25 08:58:46 -0300

    header adjusts and font size adjusts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,22 @@
 /* eslint-disable indent */
 import Modal from "../Modal/Modal.js";
 // console.log(window.location.href);
-// const url = window.location.href;
-// const page = url.split("?")[0].split("/").pop();
-// console.log(page);
+const url = window.location.href;
+const chatPage = url.split("?")[0].split("/").pop();
+console.log(chatPage);
+
+// if (page === "individual-chat" || page === "group-chat") {
+// const mobileToggle = header.querySelector("#mobile");
+// mobileToggle.classList.remove("header__toggle");
+// mobileToggle.classList.add("hide");
+// }
 
 export const Header = (data) => {
   const header = document.createElement("header");
-  // header.classList.add("header");
 
   header.innerHTML = `
           <div class="header">
-          <div class="header__logo-container">
+          <div id="headerContainer" class="header__logo-container">
           <img class="${data.img.class}" src="${data.img.src}" alt="${
     data.img.alt
   }"/>
@@ -25,7 +30,12 @@ export const Header = (data) => {
           </div>
         </div>
         <div class="header__links--desktop">
-          <a href="group-chat" class="header__link">Grupo Chat</a>
+          ${chatPage ? `<a href="/" class="header__link">Home</a>` : ``}
+          ${
+            chatPage === "group-chat"
+              ? ``
+              : `<a href="group-chat" class="header__link">Grupo Chat</a>`
+          }
           <a id="abrirModalClick" href="#" class="header__link">Chave api</a>
         </div>
         <a id="mobile" href="#" class="header__toggle">
@@ -38,22 +48,16 @@ export const Header = (data) => {
         </div>
   `;
 
-  // console.log(header.querySelector("#mobile"));
-
-  // if (page === "individual-chat" || page === "group-chat") {
-  //   const mobileToggle = header.querySelector("#mobile");
-  //   mobileToggle.classList.remove("header__toggle");
-  //   mobileToggle.classList.add("hide");
-  // }
-  // const arrowLeft = header.querySelector("#arrow-left");
-  // console.log(arrowLeft);
-  // arrowLeft.classList.add("hide");
-
   const rootElement = document.getElementById("root");
   const abrirModalClick = header.querySelector("#abrirModalClick");
   const abrirModalClickMobile = header.querySelector("#abrirModalClickMobile");
   const mobileToggle = header.querySelector("#mobile");
   const mobileLinks = header.querySelector("#myLinks");
+
+  if (chatPage === "group-chat") {
+    const headerContainer = header.querySelector("#headerContainer");
+    headerContainer.classList.add("justify-content-center");
+  }
 
   abrirModalClick.addEventListener("click", () => {
     rootElement.appendChild(Modal());
